50ab19aefb9b83227a7f5c0771c453e7af424ab7
SamuelDaigle <samuel_daigle@outlook.com>
2015-03-10 14:38:29 -0400

    Created the class Circle.

--- Shape/circle.cpp
+++ Shape/circle.cpp
@@ -5,5 +5,40 @@ using namespace ShapeLibrary;
 
 Circle::Circle(IWindowAPI& _windowAPI)
 {
+	center = NULL;
+	radius = 1;
+	windowAPI = &_windowAPI;
+	fillColor = new Color();
+	lineColor = new Color();
+}
 
+void Circle::setCenter(Point& _center)
+{
+	center = &_center;
+}
+
+void Circle::setRadius(int _radius)
+{
+	if (_radius < 0) throw invalid_argument("invalid radius: must be positive.");
+	radius = _radius;
+}
+
+void Circle::setFillColor(Color::COLOR_TYPE _colorType)
+{
+	delete fillColor;
+	fillColor = new Color(_colorType);
+}
+
+void Circle::setLineColor(Color::COLOR_TYPE _colorType)
+{
+	delete lineColor;
+	lineColor = new Color(_colorType);
+}
+
+void Circle::draw()
+{
+	if (center == NULL) throw runtime_error("circle's center must be initialized");
+	windowAPI->setDrawingColor(*fillColor);
+	windowAPI->drawCircle(*center, radius);
+	windowAPI->fillCircle(*center, radius);
 }
--- Shape/circle.h
+++ Shape/circle.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "stdafx.h"
+#include "color.h"
 
 namespace ShapeLibrary
 {
@@ -7,5 +8,19 @@ namespace ShapeLibrary
 	{
 	public:
 		Circle(IWindowAPI& _windowAPI);
+
+		void setCenter(Point& _center);
+		void setRadius(int _radius);
+		void setFillColor(Color::COLOR_TYPE _colorType);
+		void setLineColor(Color::COLOR_TYPE _colorType);
+
+		void draw();
+
+	private:
+		IWindowAPI* windowAPI;
+		Color* fillColor;
+		Color* lineColor;
+		Point* center;
+		int radius;
 	};
 }
